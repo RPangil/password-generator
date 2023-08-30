b20209c69a46152981d87142a15599a411b47c61
RPangil <rapangilinan12@gmail.com>
2023-08-29 20:06:22 -0400

    Initial Commit

---
+++ components/password-gen/passwordGen.js
@@ -0,0 +1,38 @@
+import { Fragment, useState } from "react";
+
+const PasswordGen = () =>
+{
+    const chars = [
+        'qwertyuiopasdfghjklzxcvbnm',
+        'QWERTYUIOPASDFGHJKLZXCVBNM',
+        '1234567890',
+        "!@#$%^&*()_+-=[]|{}:;,./<>?"
+    ]
+    const [genPass, setGenPass] = useState('');
+    const [upperCase, setUpperCase] = useState(false);
+    const [numbers, setNumbers] = useState(false);
+    const [symbols, setSymbols] = useState(false);
+    const [passLen, setPassLen] = useState(8);
+
+    return (
+        <Fragment>
+            <h1>Password Generator</h1>
+            <form>
+                <div>
+                    <output name='password'>{genPass}</output>
+                </div>
+                <label htmlFor='passLength'>Password Length</label>
+                <input type='number' id='passLength' name='passLength' min={8} max={16} onChange={getPassLength} value={passLen} />
+                <label htmlFor='upperCase'>Include uppercase letters</label>
+                <input type='checkbox' id='upperCase' onClick={includeUpperCase} />
+                <label htmlFor='upperCase'>Include numbers</label>
+                <input type='checkbox' id='upperCase' onClick={includeNumbers} />
+                <label htmlFor='upperCase'>Include symbols</label>
+                <input type='checkbox' id='upperCase' onClick={includeSymbols} />
+                <button type='button' onClick={generatePassword}>Generate Password</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default PasswordGen;
